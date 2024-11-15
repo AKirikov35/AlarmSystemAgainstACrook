2f7057fd6a8bc0b9a2831bdd9f1bf30cd44e1e85
AKirikov35 <161311981+AKirikov35@users.noreply.github.com>
2024-11-15 22:00:17 +0300

    Update Crook.cs

--- Crook.cs
+++ Crook.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Crook : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.useGravity = false;
+    }
 }
